4fee92b94242dd3de0e7611832fa431cc69386f2
Naveen Dulanjana <127006891+NaveeBoy@users.noreply.github.com>
2024-04-28 16:55:34 +0530

    #	modified:   restuarant/node_modules/.vite/deps/_metadata.json #	renamed:    restuarant/node_modules/.vite/deps/chunk-HZASMJ22.js -> restuarant/node_modules/.vite/deps/chunk-QI53LHDY.js #	renamed:    restuarant/node_modules/.vite/deps/chunk-HZASMJ22.js.map -> restuarant/node_modules/.vite/deps/chunk-QI53LHDY.js.map #	modified:   restuarant/node_modules/.vite/deps/react-dom_client.js #	new file:   restuarant/node_modules/.vite/deps/react-icons_gi.js #	new file:   restuarant/node_modules/.vite/deps/react-icons_gi.js.map #	modified:   restuarant/node_modules/.vite/deps/react.js #	modified:   restuarant/node_modules/.vite/deps/react_jsx-dev-runtime.js #	modified:   restuarant/node_modules/.vite/deps/react_jsx-runtime.js #	modified:   restuarant/src/Components/Container.jsx #	modified:   restuarant/src/Components/SideMenu/SideMenu.jsx

--- restuarant/src/Components/SideMenu/SideMenu.jsx
+++ restuarant/src/Components/SideMenu/SideMenu.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { GiHamburger } from "react-icons/gi";
 
 const SideMenu = () => {
   return (
     <div className='bg-sideMenuBg relative h-[100%] basis-20 p-4'>
       {/* Logo Div */}
       <div className='logo mt-4 gap-2  text-[white] flex items-center justify-center m-auto'>
-        
+      <GiHamburger className='text-[20px]'/>
+      <h1 className='text-[20px] align-center justify-center font-black'> Foodie</h1>
+      </div>
+
+      {/* User Div */}
+      <div className='flex items-center justify-center text-[white] flex-col mt-[3rem] admin '>
+        <img src="" alt="Admin-Image"  className='h-[80px] w-[80px] border-red-200 border-[4px] object-cover rounded-full'/>
+        <span className='opacity-70 mt-2 text-gray-400'>Welcome,</span>
+        <h3 className='font-bold text-textColor'>Anna Lindsey</h3>
       </div>
     </div>
   )
